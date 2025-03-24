0cb09b022140cd3e8c3ed101ace3c70ca26a8745
ashokvarmad <137379117+ashokvarmad@users.noreply.github.com>
2025-03-24 22:25:28 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "ashok-aws"
+}
+
+resource "aws_instance" "ashok_app" {
+  ami           = "ami-0d0f28110d16ee7d6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AshokAppServer"
+    Owner = "Ashok" 
+  }
+}
